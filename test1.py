bc4d248cde356b7f55a231b96922c788060df386
Aahan <mehta295@purdue.edu>
2025-05-24 23:12:41 +0530

    updated test1.py

--- test1.py
+++ test1.py
@@ -2,6 +2,12 @@ import cv2
 import numpy as np
 import mediapipe as mp
 
+mp_pose = mp.solutions.pose # loading the model (Pose)
+pose = mp_pose.Pose() # initializes it
+points = mp_pose.PoseLandmark # provides constants 
+mp_drawing = mp.solutions.drawing_utils
+
+
 cam = cv2.VideoCapture(0)
 
 if not cam.isOpened():
@@ -10,9 +16,14 @@ if not cam.isOpened():
 
 while True:
     ret, frame = cam.read()
+
+    results = pose.process(frame)
+    # print(results.pose_landmarks)
+    mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
     cv2.imshow('window name', frame)
 
-    if cv2.waitkey(1) == orq('q'):
+    if cv2.waitKey(1) == ord('q'):
         break
         
 cam.release()
