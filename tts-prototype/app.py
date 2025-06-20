49d29712617a77174d4daea751a34773daa54003
Gia-Gupta <121402055+Gia-Gupta@users.noreply.github.com>
2025-06-20 09:33:10 -0700

    websocket audio

--- tts-prototype/app.py
+++ tts-prototype/app.py
@@ -1,22 +1,28 @@
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, render_template_string
+from flask_socketio import SocketIO, emit
 import pyttsx3
 import tempfile
+import base64
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     with open('index.html') as f:
         return render_template_string(f.read())
 
-@app.route('/api/tts', methods=['POST'])
-def tts_api():
-    text = request.json['text']
+@socketio.on('tts_request')
+def handle_tts_request(data):
+    text = data['text']
     engine = pyttsx3.init()
     tmp = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
     engine.save_to_file(text, tmp.name)
     engine.runAndWait()
-    return send_file(tmp.name, mimetype='audio/mpeg')
+    with open(tmp.name, "rb") as f:
+        audio_data = f.read()
+    audio_b64 = base64.b64encode(audio_data).decode('utf-8')
+    emit('tts_response', {'audio': audio_b64})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
